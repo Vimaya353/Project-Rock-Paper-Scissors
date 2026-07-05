0a1a29d21f4043114f67c340854972132ad339b3
vimaya <arionlester88@gmail.com>
2026-07-05 19:05:29 +0530

    Create a new function named getHumanChoice so that it will return one of the valid choices depending on what the user inputs.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
-    
+
     if (randomNumber === 0) {
     return "rock";
   } else if (randomNumber === 1) {
@@ -9,3 +9,8 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+function getHumanChoice() {
+  return prompt("Enter rock, paper, or scissors:");
+}
+
